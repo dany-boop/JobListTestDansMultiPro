add337f40342f1afeab43fe1fd11054ed95f6152
dany-boop <dny77853@gmail.com>
2023-07-12 16:09:58 +0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
       <GoogleOAuthProvider clientId="866932345241-320c1fllsot5g3jbb1748lied0rar157.apps.googleusercontent.com">
         <Routes>
           <Route exact path="/" name="Login" element={<Login />} />
+        </Routes>
+        <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route exact path="summary/:id" name="Detail" element={<Detail />} />
         </Routes>
